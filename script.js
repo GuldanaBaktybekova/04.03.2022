bf4e615ef9d538057546424ac8b97659ed5e2ba0
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-04 15:54:27 +0600

    Added clearInterval

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-const darkTheme = document.querySelector('input[type=checkbox]');
+const span = document.querySelector("span");
+let interval = setInterval(() => {
+  span.textContent++;
+}, 1);
 
-darkTheme.checked = +localStorage.getItem('dark-theme');
-document.body.classList.toggle('dark-theme', darkTheme.checked);
-
-darkTheme.addEventListener('change', function(){
-  localStorage.setItem('dark-theme', +darkTheme.checked);
-  document.body.classList.toggle('dark-theme');
+document.querySelector("button").addEventListener("click", function () {
+  clearInterval(interval);
 });
 
+// let timeoutId;
+// document.querySelector('button').addEventListener('click', function(){
+//   this.textContent = "Generating...";
+//   clearTimeout(timeoutId);
+//   timeoutId = setTimeout(()=> {
+//     this.textContent = Math.random() * 100;
+//   }, 3000);
+// });
 
+// const timeoutExample = setTimeout(function() {
+//   document.querySelector('span').textContent = "Hello";
+// }, 3000);
+// document.querySelector('button').addEventListener('click', function(){
+//   clearTimeout(timeoutExample);
+// });
 
+// const darkTheme = document.querySelector('input[type=checkbox]');
 
+// darkTheme.checked = +localStorage.getItem('dark-theme');
+// document.body.classList.toggle('dark-theme', darkTheme.checked);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// darkTheme.addEventListener('change', function(){
+//   localStorage.setItem('dark-theme', +darkTheme.checked);
+//   document.body.classList.toggle('dark-theme');
+// });
 
 // const example1span = document.querySelector('#examle-1 span');
 // example1Span.textContent = localStorage.getItem('example-1-number') ?? 0;
@@ -36,10 +42,8 @@ darkTheme.addEventListener('change', function(){
 //     localStorage.setItem('example-1-number', example1Span.textContent);
 //   });
 
-
 // const example3Input = document.querySelector('#example-3 input');
 // example3Input.value - localStorage.getItem('example-3-input');
 // example3Input.addEventListener('input', function(){
 //   localStorage.setItem('example-3-input', example3Input.value);
 // });
-
